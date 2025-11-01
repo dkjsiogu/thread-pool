e5053133742b8226f082cbd7e9e77b6a4085db13
dkjsiogu <15715022545@163.com>
2025-11-01 21:16:45 +0800

    2

--- src/My_test/My_thread_pool.cpp
+++ src/My_test/My_thread_pool.cpp
@@ -1,7 +1,25 @@
 #include "My_thread_pool.hpp"
 thread_pool::MyThreadPool::MyThreadPool(size_t num_threads)
 {
-    for(size_t i=0;i<num_threads;++i){
-        workers_=
+    for (size_t i = 0; i < num_threads; ++i)
+    {
+        workers_.emplace_back([this, i](){
+            std::cout<<"start "<<i<<std::endl;
+            while (true){
+                std::function<void()> task;{
+                    
+                    {
+                    std::unique_lock<std::mutex> lock(queue_mutex_);
+                    condition_.wait(lock,[this](){
+                        return stop_||tasks_.empty();
+                        });
+                    
+                    }
+
+                    
+                    
+                    }
+                }
+            });
     }
 }
